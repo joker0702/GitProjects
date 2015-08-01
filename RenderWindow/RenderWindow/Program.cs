9b18109f6b3a859731d7b4555c04ea60b80bdd0e
oe2dlo <thomas.josef.koch@gmail.com>
2015-08-01 16:36:46 +0200

    Add Program.cs

---
+++ RenderWindow/RenderWindow/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX.Windows;
+using SharpDX.DirectInput;
+
+namespace RenderWindow
+{
+    class Program
+    {
+        private static RenderForm form;
+
+        static void Main(string[] args)
+        {
+            using (form = new RenderForm("My first rendered Window"))
+            {
+                RenderLoop.Run(form, RenderCallback);
+            }
+        }
+
+        private static void RenderCallback()
+        {
+            Console.Clear();
+            DirectInput directInput = new DirectInput();
+            Keyboard keyboard = new Keyboard(directInput);
+            keyboard.SetCooperativeLevel(form, CooperativeLevel.Background | CooperativeLevel.NonExclusive);
+            keyboard.Acquire();
+            KeyboardState keyState = keyboard.GetCurrentState();
+
+            if (keyState.IsPressed(Key.Escape))
+            {
+                form.Close();
+                form.Dispose();
+            }
+        }
+    }
+}
